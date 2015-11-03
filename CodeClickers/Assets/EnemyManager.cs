ab8eaeb95450713204548ff20ff6a8312c22ab35
davi3145 <davi3145@vodskov-skole.dk>
2015-11-03 16:50:24 +0100

    Added Player, Enemy and EnemyManager

---
+++ CodeClickers/Assets/EnemyManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnemyManager : MonoBehaviour {
+
+	public Enemy currentEnemy;
+	public Image[] images;
+
+	public void ApplyDamage (int damage) {
+		currentEnemy.ReceiveDamage(damage);
+	}
+
+	public void createEnemy() {
+		print ("New enemy");
+		currentEnemy.Init(5, 1, images[Random.Range(0, images.Length)]);
+	}
+
+	void Start () {
+		print ("Test");
+	}
+
+	void Update () {
+	
+	}
+}
---
+++ CodeClickers/Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public EnemyManager enemyManager;
+	public int clickDamage = 1;
+	public int damagePerSecond = 0;
+
+	private IEnumerator Start() {
+		while(true) {
+			yield return new WaitForSeconds(1);
+			GiveDPS();
+		}
+	}
+
+	public void GiveClickDamage () {
+		enemyManager.ApplyDamage(clickDamage);
+	}
+
+	private void GiveDPS () {
+		enemyManager.ApplyDamage(damagePerSecond);
+	}
+
+	void Update () {
+	
+	}
+}
